4352618cd917967b46ccfd1b3077053c78b5b4dd
vvf03 <87320310+vvf03@users.noreply.github.com>
2021-07-18 19:28:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,58 @@
 #include <fstream>
 #include <list>
 #include <string>
-
-int main()
+using namespace std;
+int main(int argc, char *argv[])
 {
-    std::fstream ifs("C:/Users/Vlad/Documents/untitled2/text", std::ios::in), ofs("C:/Users/Vlad/Documents/untitled2/text2", std::ios::out);
+    string ifs;
+    string ofs;
+    string str;
+    list<string> lst;
 
-        std::string str;
-        std::list<std::string> lst;
-        while (std::getline(ifs, str))
+    //запрос пути к считываемому файлу
+    if (argc==2)
+    {
+        ifs=argv[1];
+    }
+    else
+    {
+        cout<<"Enter path to file"<<endl;
+        cin>>ifs;
+    }
+
+    if (argc==3)
+    {
+        ofs=argv[2];
+    }
+    else
+    {
+        cout<<"Enter path from file"<<endl;
+        cin>>ofs;
+    }
+
+        fstream in;
+        in.open(ifs);
+        while (getline(in, str))
         {
             lst.push_front(str);
         }
         for (auto const& str : lst)
         {
-            ofs << str << '\n';
+            cout<<str<<'\n';
         }
+        in.close();
 
+        cout<<" "<<endl;
+        ofstream out;
+        out.open(ofs);
+        while (getline(in, str))
+        {
+            lst.push_front(str);
+        }
+        for (auto const& str : lst)
+        {
+            cout<<str<<'\n';
+        }
+        out.close();
     return 0;
 }
